7475043ee86d7fa5536cf64148df6d6cef6adad6
Lance Salmingo <salmingolance@gmail.com>
2025-11-07 22:04:48 +1100

    Full working website with dataset files

---
+++ datasets/regional_genres.js
@@ -0,0 +1,217 @@
+window.REGIONAL_GENRE_DATA = {
+  "North America": {
+    "Drama": {
+      "popularity": 826.932,
+      "avg_vote": 5.047,
+      "count": 16
+    },
+    "Action": {
+      "popularity": 167.862,
+      "avg_vote": 4.928,
+      "count": 8
+    },
+    "Comedy": {
+      "popularity": 29.512,
+      "avg_vote": 6.062,
+      "count": 1
+    },
+    "Biography": {
+      "popularity": 3.976,
+      "avg_vote": 5.5,
+      "count": 1
+    },
+    "Adventure": {
+      "popularity": 4.527,
+      "avg_vote": 6.593,
+      "count": 1
+    },
+    "Documentary": {
+      "popularity": 19.845,
+      "avg_vote": 5.072,
+      "count": 7
+    }
+  },
+  "South America": {
+    "Drama": {
+      "popularity": 480.496,
+      "avg_vote": 5.025,
+      "count": 12
+    },
+    "Documentary": {
+      "popularity": 24.987,
+      "avg_vote": 5.431,
+      "count": 2
+    }
+  },
+  "Europe": {
+    "Drama": {
+      "popularity": 3429.055,
+      "avg_vote": 4.976,
+      "count": 42
+    },
+    "Action": {
+      "popularity": 40.559,
+      "avg_vote": 4.516,
+      "count": 2
+    },
+    "Documentary": {
+      "popularity": 17.352,
+      "avg_vote": 3.642,
+      "count": 1
+    },
+    "Adventure": {
+      "popularity": 0.014,
+      "avg_vote": 0,
+      "count": 1
+    },
+    "Biography": {
+      "popularity": 4.884,
+      "avg_vote": 7.123,
+      "count": 2
+    }
+  },
+  "East Asia": {
+    "Drama": {
+      "popularity": 731.096,
+      "avg_vote": 4.809,
+      "count": 6
+    },
+    "Action": {
+      "popularity": 168.979,
+      "avg_vote": 5.428,
+      "count": 2
+    }
+  },
+  "South Asia": {
+    "Drama": {
+      "popularity": 286.362,
+      "avg_vote": 4.638,
+      "count": 6
+    },
+    "Comedy": {
+      "popularity": 6.061,
+      "avg_vote": 6.892,
+      "count": 1
+    },
+    "Adventure": {
+      "popularity": 5.843,
+      "avg_vote": 6.4,
+      "count": 1
+    }
+  },
+  "Southeast Asia": {
+    "Comedy": {
+      "popularity": 5.353,
+      "avg_vote": 5.76,
+      "count": 1
+    },
+    "Adventure": {
+      "popularity": 45.508,
+      "avg_vote": 4.077,
+      "count": 2
+    },
+    "Drama": {
+      "popularity": 352.495,
+      "avg_vote": 4.24,
+      "count": 6
+    },
+    "Documentary": {
+      "popularity": 6.512,
+      "avg_vote": 3.95,
+      "count": 1
+    },
+    "Biography": {
+      "popularity": 0.046,
+      "avg_vote": 0,
+      "count": 1
+    }
+  },
+  "Central Asia": {
+    "Drama": {
+      "popularity": 65.263,
+      "avg_vote": 4.662,
+      "count": 4
+    },
+    "Adventure": {
+      "popularity": 1.943,
+      "avg_vote": 7.0,
+      "count": 1
+    }
+  },
+  "Middle East": {
+    "Drama": {
+      "popularity": 500.092,
+      "avg_vote": 4.75,
+      "count": 17
+    },
+    "Crime": {
+      "popularity": 18.136,
+      "avg_vote": 5.67,
+      "count": 1
+    }
+  },
+  "Africa": {
+    "Drama": {
+      "popularity": 671.887,
+      "avg_vote": 5.337,
+      "count": 29
+    },
+    "Adventure": {
+      "popularity": 35.088,
+      "avg_vote": 5.824,
+      "count": 4
+    },
+    "Documentary": {
+      "popularity": 81.975,
+      "avg_vote": 5.272,
+      "count": 14
+    },
+    "Action": {
+      "popularity": 51.345,
+      "avg_vote": 5.248,
+      "count": 2
+    },
+    "Crime": {
+      "popularity": 3.06,
+      "avg_vote": 4.2,
+      "count": 1
+    },
+    "Comedy": {
+      "popularity": 2.666,
+      "avg_vote": 5.8,
+      "count": 1
+    },
+    "Thriller": {
+      "popularity": 2.919,
+      "avg_vote": 4.346,
+      "count": 1
+    }
+  },
+  "Oceania": {
+    "Documentary": {
+      "popularity": 10.031,
+      "avg_vote": 5.084,
+      "count": 7
+    },
+    "Drama": {
+      "popularity": 305.264,
+      "avg_vote": 6.104,
+      "count": 3
+    },
+    "Comedy": {
+      "popularity": 2.315,
+      "avg_vote": 4.0,
+      "count": 1
+    },
+    "Adventure": {
+      "popularity": 11.929,
+      "avg_vote": 7.264,
+      "count": 5
+    },
+    "Action": {
+      "popularity": 7.17,
+      "avg_vote": 5.3,
+      "count": 3
+    }
+  }
+};
